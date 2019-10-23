dd3ada7e906c58eca1b99765615f8f12f77b30b2
dnische <danilchenkoy@gmail.com>
2019-10-23 18:23:03 -0400

    Update MyUntitledTest.java

--- src/test/java/MyUntitledTest.java
+++ src/test/java/MyUntitledTest.java
@@ -1,13 +1,9 @@
-package com.example;
-
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -36,28 +32,6 @@ public class MyUntitledTest {
         driver.findElement(By.name("q")).sendKeys("seleniumqh");
         driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
         assertEquals("Search results page title", "seleniumqh - Google Search", driver.getTitle());
-        driver.findElement(By.xpath("//span[.='Showing results for']/..//i")).click();
-        assertEquals("Search results page title", "seleniumqh - Google Search", driver.getTitle());
-        driver.findElement(By.xpath("//h3[.='Selenium - Web Browser Automation']")).click();
-        assertEquals("SeleniumHQ page title", "Selenium - Web Browser Automation", driver.getTitle());
-    }
-
-    @Test
-    public void myUntitledExpanded() {
-        assertEquals("Home page title", "Google", driver.getTitle());
-
-
-//        Define and initialize locator
-        By searchInputLocator = By.name("q");
-//        Find the element on the page
-        WebElement searchInput = driver.findElement(searchInputLocator);
-//        Set the text value to the element
-        searchInput.sendKeys("seleniumqh");
-//        submit search by hitting Enter in the search input
-        searchInput.sendKeys(Keys.ENTER);
-        assertEquals("Search results page title", "seleniumqh - Google Search", driver.getTitle());
-
-
         driver.findElement(By.xpath("//span[.='Showing results for']/..//i")).click();
         assertEquals("Search results page title", "seleniumqh - Google Search", driver.getTitle());
         driver.findElement(By.xpath("//h3[.='Selenium - Web Browser Automation']")).click();
