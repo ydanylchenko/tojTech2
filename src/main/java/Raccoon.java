6085be4b027843783e6622ecc20a578a48683cb9
dnische <danilchenkoy@gmail.com>
2019-10-28 20:44:45 -0400

    Day5 - Add Raccoon and Zoo classes

---
+++ src/main/java/Raccoon.java
@@ -0,0 +1,30 @@
+public class Raccoon {
+    String name;
+    int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    double weight;
+}
