b194b621ebb74daefe7c584ab272c38e41eace77
dnische <danilchenkoy@gmail.com>
2019-10-30 20:41:18 -0400

    Day6 - Update packages

---
+++ src/main/java/com/theladders/myAccount/MyAccountBilling.java
@@ -0,0 +1,4 @@
+package com.theladders.myAccount;
+
+public class MyAccountBilling {
+}
--- src/main/java/org/craigslist/CraigsListMainPage.java
+++ src/main/java/org/craigslist/CraigsListMainPage.java
@@ -5,8 +5,6 @@ import com.amazon.AmazonBasePage;
 public class CraigsListMainPage {
 
     public static void main(String[] args) {
-//        Specifying the whole path
-        com.amazon.AmazonBasePage amazonBasePage = new AmazonBasePage();
 //        Using import
         AmazonBasePage amazonBasePageOne = new AmazonBasePage();
     }
