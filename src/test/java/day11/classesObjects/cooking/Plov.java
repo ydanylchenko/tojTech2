8844c95b36876949548c9c0c5dd40e5e4f56f4df
dnische <danilchenkoy@gmail.com>
2019-11-13 21:18:40 -0500

    Add plov

--- src/test/java/day11/classesObjects/cooking/Plov.java
+++ src/test/java/day11/classesObjects/cooking/Plov.java
@@ -11,15 +11,19 @@ public class Plov {
         cutVegetables(vegetablesIngredient);
     }
 
-    private void cutVegetables(Vegetables unCutVegetables){
+    private void mixItUp(Meat meatIngredient, Rice riceIngredient, Vegetables vegetablesIngredient) {
+        System.out.println("We are mixing all the stuff: " + meatIngredient + riceIngredient + vegetablesIngredient);
+    }
+
+    private void cutVegetables(Vegetables unCutVegetables) {
         System.out.println("Vegetables are being cut " + unCutVegetables);
     }
 
-    private void cookMeat(Meat rawMeat){
+    private void cookMeat(Meat rawMeat) {
         System.out.println("Meat is cooked " + rawMeat);
     }
 
-    private void washRice(Rice riceIngredient){
+    private void washRice(Rice riceIngredient) {
         System.out.println("Rice is being washed: " + riceIngredient);
     }
 }
