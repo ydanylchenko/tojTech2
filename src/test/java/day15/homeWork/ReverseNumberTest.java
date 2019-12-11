834f23ca37a326be1f7f360ed7b3910f946b5e94
dnische <danilchenkoy@gmail.com>
2019-12-11 18:49:27 -0500

    Revert changes

--- src/test/java/day15/homeWork/ReverseNumberTest.java
+++ src/test/java/day15/homeWork/ReverseNumberTest.java
@@ -1,7 +1,6 @@
 package day15.homeWork;
 
 import org.junit.Test;
-import org.openqa.selenium.json.JsonOutput;
 
 import static org.junit.Assert.assertEquals;
 
@@ -14,38 +13,33 @@ public class ReverseNumberTest {
      */
     public static int getReversedDigit(int digit) {
         int reversedDigit = digit;
-
-        digit = Math.abs(digit);
-        String digits = new StringBuilder(String.valueOf(digit)).reverse().toString();
-        reversedDigit = Integer.valueOf(digits);
+//        Implementation goes here.
         return reversedDigit;
     }
 
-
-        @Test
-        public void positiveDigitTest () {
-
-            assertEquals(1, getReversedDigit(1));
-            assertEquals(321, getReversedDigit(123));
-        }
-
-        @Test
-        public void negativeDigitTest () {
-            assertEquals(1, getReversedDigit(-1));
-        }
-
-        @Test
-        public void zeroTest () {
-            assertEquals(0, getReversedDigit(0));
-        }
-
-        @Test
-        public void bigValueTest () {
-            assertEquals(76723, getReversedDigit(Short.MAX_VALUE));
-        }
-
-        @Test
-        public void smallValueTest () {
-            assertEquals(86723, getReversedDigit(Short.MIN_VALUE));
-        }
+    @Test
+    public void positiveDigitTest() {
+        assertEquals(1, getReversedDigit(1));
+        assertEquals(321, getReversedDigit(123));
     }
+
+    @Test
+    public void negativeDigitTest() {
+        assertEquals(1, getReversedDigit(-1));
+    }
+
+    @Test
+    public void zeroTest() {
+        assertEquals(0, getReversedDigit(0));
+    }
+
+    @Test
+    public void bigValueTest() {
+        assertEquals(76723, getReversedDigit(Short.MAX_VALUE));
+    }
+
+    @Test
+    public void smallValueTest() {
+        assertEquals(86723, getReversedDigit(Short.MIN_VALUE));
+    }
+}
